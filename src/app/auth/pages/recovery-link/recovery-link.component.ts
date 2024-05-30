9f594af70bb4b8ecd0326ca86972d6f3f75a47f2
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-30 18:42:16 -0500

    feat(auth): generate 'recovery-link' component

---
+++ src/app/auth/pages/recovery-link/recovery-link.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recovery-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './recovery-link.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RecoveryLinkComponent {}
