a29a6eff9196e67bbdd2602a85176a33175d9397
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-29 20:42:09 -0500

    feat(auth): generate 'social-wrapper' component

---
+++ src/app/auth/shared/social-wrapper/social-wrapper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-wrapper',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-wrapper.component.html',
+})
+export class SocialWrapperComponent {}
