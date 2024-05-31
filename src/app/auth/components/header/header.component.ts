f684d448ff9635c0b332bce800ec303d3df61f39
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-31 16:01:57 -0500

    feat(auth): configure 'title' input signal

--- src/app/auth/components/header/header.component.ts
+++ src/app/auth/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TrelloLogoComponent } from '../../../shared/components/trello-logo/trello-logo.component';
 
 @Component({
@@ -8,4 +8,6 @@ import { TrelloLogoComponent } from '../../../shared/components/trello-logo/trel
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public title = input.required<string>();
+}
