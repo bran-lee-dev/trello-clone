ea1e4d07c47b31c9162ba9756ec409210f230696
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-05-31 15:55:04 -0500

    feat(footer.component.ts): import 'AtlassianLogoComponent' component

--- src/app/auth/components/footer/footer.component.ts
+++ src/app/auth/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AtlassianLogoComponent } from '../../../shared/components/atlassian-logo/atlassian-logo.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [AtlassianLogoComponent],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
