ea77caff7373da786837979793a87b2ee60c173e
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-01 15:38:17 -0500

    feat(auth-error-icon.component.ts): import 'NgStyle' and configure signal input

--- src/app/shared/components/auth-error-icon/auth-error-icon.component.ts
+++ src/app/shared/components/auth-error-icon/auth-error-icon.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-error-icon',
   standalone: true,
+  imports: [NgStyle],
   templateUrl: './auth-error-icon.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthErrorIconComponent {}
+export class AuthErrorIconComponent {
+  public fillColor = input.required<string>();
+}
