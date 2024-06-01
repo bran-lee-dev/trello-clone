21cee8102830c2a5673931e96fe87c4ef55fad79
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-01 12:53:51 -0500

    feat(auth): export 'error-message.interface.ts' file

--- src/app/auth/models/index.ts
+++ src/app/auth/models/index.ts
@@ -1,2 +1,3 @@
 export type { SocialNetwork } from './interfaces/social-network.interface';
 export type { NavigationLink } from './interfaces/navigation-link.interface';
+export type { ErrorMessages } from './interfaces/error-messages.interface';
