32279a1f588b69db86d5899ee027fce7c2f31ffd
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-01 19:55:04 -0500

    feat(auth): export 'legal-link.interface.ts' file

--- src/app/auth/models/index.ts
+++ src/app/auth/models/index.ts
@@ -1,3 +1,4 @@
 export type { SocialNetwork } from './interfaces/social-network.interface';
 export type { NavigationLink } from './interfaces/navigation-link.interface';
 export type { ErrorMessages } from './interfaces/error-messages.interface';
+export type { LegalLink } from './interfaces/legal-link.interface';
