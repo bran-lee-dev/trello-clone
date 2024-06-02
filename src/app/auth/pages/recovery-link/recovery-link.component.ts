6490b1ad364b0db247bd102cf031c3e7eb03ea16
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-01 20:21:55 -0500

    feat(recovery-link): add layout, styles and all configuration

--- src/app/auth/pages/recovery-link/recovery-link.component.ts
+++ src/app/auth/pages/recovery-link/recovery-link.component.ts
@@ -1,10 +1,50 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AuthFormComponent } from '../../components/auth-form/auth-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { LegalLinksComponent } from '../../components/legal-links/legal-links.component';
+import { NavigationLinksComponent } from '../../components/navigation-links/navigation-links.component';
+import { NavigationLink, ErrorMessages, LegalLink } from '../../models';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-recovery-link',
   standalone: true,
-  imports: [],
+  imports: [
+    AuthFormComponent,
+    FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
+    LegalLinksComponent,
+    NavigationLinksComponent,
+  ],
   templateUrl: './recovery-link.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecoveryLinkComponent {}
+export class RecoveryLinkComponent {
+  public readonly navigationLinksList: NavigationLink[] = [
+    {
+      label: 'Return to log in',
+      routerLink: '/login',
+    },
+  ];
+
+  public readonly errorMessages: ErrorMessages = {
+    required: 'Please enter an email address',
+    email: 'Please enter a valid email address',
+  };
+
+  public readonly legalLinksList: LegalLink[] = [
+    {
+      label: 'Login help',
+      link: 'https://support.atlassian.com/atlassian-account/docs/troubleshoot-login-issues-with-your-atlassian-account/',
+    },
+    {
+      label: '•',
+    },
+    {
+      label: 'Contact Support',
+      link: 'https://support.atlassian.com/',
+    },
+  ];
+}
