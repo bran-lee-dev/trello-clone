e1e16afdf37f49c83d5039f94a209fd83b81fc0a
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-04 14:15:19 -0500

    feat(social-wrapper): implement image optimization

--- src/app/auth/components/social-wrapper/social-wrapper.component.ts
+++ src/app/auth/components/social-wrapper/social-wrapper.component.ts
@@ -1,11 +1,29 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
+import {
+  IMAGE_LOADER,
+  ImageLoaderConfig,
+  NgOptimizedImage,
+  PRECONNECT_CHECK_BLOCKLIST,
+  TitleCasePipe,
+} from '@angular/common';
 import { SocialNetwork } from '../../models';
 
 @Component({
   selector: 'app-social-wrapper',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, NgOptimizedImage],
+  providers: [
+    {
+      provide: PRECONNECT_CHECK_BLOCKLIST,
+      useValue: 'https://id-frontend.prod-east.frontend.public.atl-paas.net',
+    },
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        return `https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/${config.src}`;
+      },
+    },
+  ],
   templateUrl: './social-wrapper.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
