b7729779ecf398f56402d5f30bc667cd07400adf
Brandon Agüero <brandonaguerodeveloper@gmail.com>
2024-06-05 20:31:49 -0500

    feat(auth.routes.ts): add 'title' property for all routes with content

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 export const AUTH_ROUTES: Routes = [
   {
     path: 'login',
+    title: 'Log in to continue - Log in with Atlassian account',
     loadComponent: () =>
       import('./pages/login/login.component').then(m => m.LoginComponent),
   },
   {
     path: 'signup',
+    title: 'Sign up - Log in with Atlassian account',
     loadComponent: () =>
       import('./pages/register/register.component').then(
         m => m.RegisterComponent
@@ -15,6 +17,7 @@ export const AUTH_ROUTES: Routes = [
   },
   {
     path: 'resetpassword',
+    title: `Can't log in? - Log in with Atlassian account`,
     loadComponent: () =>
       import('./pages/recovery-link/recovery-link.component').then(
         m => m.RecoveryLinkComponent
